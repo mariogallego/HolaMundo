2533c57a8660123e5e68dc3c14040b601e9b4a2f
mariogallego <mgallego.2991@gmail.com>
2022-02-08 18:56:36 +0100

    Comentario creado desde github

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -3,6 +3,7 @@ package com.acme;
 public record PersonaRegistro(String nombre, String apellidos) {
     @Override
     public String toString() {
+        //Linea comentada
         return "PersonaRegistro{" +
                 "nombre='" + nombre + '\'' +
                 ", apellidos='" + apellidos + '\'' +
